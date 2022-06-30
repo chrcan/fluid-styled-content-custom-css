17db798ddf8045790875559b45d057559362ef5e
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2022-06-30 15:44:42 +0200

    [TASK] Add missing dependency and fix version

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
+            'fluid_styled_content' => '11.5.0-11.5.99',
         ],
         'conflicts' => [
         ],
