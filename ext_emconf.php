c15d741c9e7574919e61e99951e97438804eafad
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2022-06-30 15:45:24 +0200

    [TASK] Change category and version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,11 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Additional tt_content css class field',
     'description' => 'Add CSS Class in Text & Media on Image',
-    'category' => 'plugin',
+    'category' => 'fe',
     'author' => 'Christian Racan',
     'author_email' => 'werbegrafik@rc-design.at',
     'state' => 'alpha',
-    'version' => '1.0.0',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
