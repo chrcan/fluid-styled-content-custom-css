f47d17b89dcedddabc4f0954b70f219ddd196e0a
Christian <werbegrafik@rc-design.at>
2022-06-30 15:24:13 +0200

    exclude

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,11 +1,12 @@
 <?php
+
 defined('TYPO3_MODE') or die();
 
 // Configure new fields:
 $fields = [
     'tx_ttcontentaddfields_customcss' => [
         'label' => 'LLL:EXT:ttcontent_addfields/Resources/Private/Language/locallang_db.xlf:tt_content.tx_ttcontentaddfields_customcss',
-        'exclude' => 1,
+        'exclude' => 0,
         'config' => [
             'type' => 'input',
             'max' => 255
@@ -19,13 +20,14 @@ $fields = [
 // Make fields visible in the TCEforms:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content', // Table name
-    '--palette--;LLL:EXT:ttcontent_addfields/Resources/Private/Language/locallang_db.xlf:tt_content.palette_title;tx_ttcontentaddfields',
+    '--palette--;;tx_ttcontentaddfields',
     // Field list to add
-    '1', // List of specific types to add the field list to. (If empty, all type entries are affected)
-    'after:title' // Insert fields before (default) or after one, or replace a field
+    '', // List of specific types to add the field list to. (If empty, all type entries are affected)
+    'after:space_after_class' // Insert fields before (default) or after one, or replace a field
 );
 
 // Add the new palette:
 $GLOBALS['TCA']['tt_content']['palettes']['tx_ttcontentaddfields'] = [
+    'label' => 'LLL:EXT:ttcontent_addfields/Resources/Private/Language/locallang_db.xlf:tt_content.palette_title',
     'showitem' => 'tx_ttcontentaddfields_customcss'
 ];
