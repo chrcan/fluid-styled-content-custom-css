9331a87d52ad63b4dac9330f01d5845a077bbef5
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2022-07-01 16:49:50 +0200

    [TASK] Change extension key (#8)

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -26,8 +26,8 @@ defined('TYPO3') or die();
 // Replace FSC static include
 foreach ($GLOBALS['TCA']['sys_template']['columns']['include_static_file']['config']['items'] as &$itemArray) {
     if ($itemArray[0] === 'Fluid Content Elements (fluid_styled_content)') {
-        $itemArray[0] = 'Fluid Content Elements (rcd_fluid_styled_content_custom_css)';
-        $itemArray[1] = 'EXT:rcd_fluid_styled_content_custom_css/Configuration/TypoScript/';
+        $itemArray[0] = 'Fluid Content Elements (rcd_fsc_custom_css)';
+        $itemArray[1] = 'EXT:rcd_fsc_custom_css/Configuration/TypoScript/';
         break;
     }
 }
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -25,8 +25,8 @@ defined('TYPO3_MODE') or die();
 
 // Configure new fields:
 $fields = [
-    'tx_rcd_fluid_styled_content_custom_css_class' => [
-        'label' => 'LLL:EXT:rcd_fluid_styled_content_custom_css/Resources/Private/Language/locallang_db.xlf:tt_content.custom_css_class',
+    'tx_rcd_fsc_custom_css_class' => [
+        'label' => 'LLL:EXT:rcd_fsc_custom_css/Resources/Private/Language/locallang_db.xlf:tt_content.custom_css_class',
         'exclude' => 0,
         'config' => [
             'type' => 'input',
@@ -41,14 +41,14 @@ $fields = [
 // Make fields visible in the TCEforms:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content', // Table name
-    '--palette--;;rcd_fluid_styled_content_custom_css',
+    '--palette--;;rcd_fsc_custom_css',
     // Field list to add
     '', // List of specific types to add the field list to. (If empty, all type entries are affected)
     'after:space_after_class' // Insert fields before (default) or after one, or replace a field
 );
 
 // Add the new palette:
-$GLOBALS['TCA']['tt_content']['palettes']['rcd_fluid_styled_content_custom_css'] = [
-    'label' => 'LLL:EXT:rcd_fluid_styled_content_custom_css/Resources/Private/Language/locallang_db.xlf:tt_content.custom_css_palette_title',
-    'showitem' => 'tx_rcd_fluid_styled_content_custom_css_class',
+$GLOBALS['TCA']['tt_content']['palettes']['rcd_fsc_custom_css'] = [
+    'label' => 'LLL:EXT:rcd_fsc_custom_css/Resources/Private/Language/locallang_db.xlf:tt_content.custom_css_palette_title',
+    'showitem' => 'tx_rcd_fsc_custom_css_class',
 ];
