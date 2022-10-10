378e660113ccff64616c111b893a1e7edd85a04f
Christian <werbegrafik@rc-design.at>
2022-10-10 10:46:00 +0200

    v12update

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'author' => 'Christian Racan',
     'author_email' => 'werbegrafik@rc-design.at',
-    'state' => 'alpha',
-    'version' => '0.1.0',
+    'state' => 'stable',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'fluid_styled_content' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.1.99',
+            'fluid_styled_content' => '11.5.0-12.1.99',
         ],
         'conflicts' => [
         ],
