1e5c6e209f48086c4b60c329e2c26f5531c6d8cc
Christian <werbegrafik@rc-design.at>
2023-02-09 09:12:41 +0100

    cms-core & fsc update v12.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,12 +31,10 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.1.99',
-            'fluid_styled_content' => '11.5.0-12.1.99',
+            'typo3' => '11.5.0-12.3.99',
+            'fluid_styled_content' => '11.5.0-12.3.99',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
 ];
