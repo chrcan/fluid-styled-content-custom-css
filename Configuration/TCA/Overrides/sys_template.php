e970a4b357c4ed47e88444ff3b6c74db6e6b71cc
chrcan <53138963+chrcan@users.noreply.github.com>
2023-04-06 09:56:42 +0200

    Update (#11) (#12)
    remove Replace FSC static include in sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -29,10 +29,10 @@ defined('TYPO3') or die();
     'FSC-Custom-Css'
 );
 // Replace FSC static include
-foreach ($GLOBALS['TCA']['sys_template']['columns']['include_static_file']['config']['items'] as &$itemArray) {
-    if ($itemArray[0] === 'Fluid Content Elements (fluid_styled_content)') {
-        $itemArray[0] = 'Fluid Content Elements (rcd_fsc_custom_css)';
-        $itemArray[1] = 'EXT:rcd_fsc_custom_css/Configuration/TypoScript/';
-        break;
-    }
-}
+// foreach ($GLOBALS['TCA']['sys_template']['columns']['include_static_file']['config']['items'] as &$itemArray) {
+//     if ($itemArray[0] === 'Fluid Content Elements (fluid_styled_content)') {
+//         $itemArray[0] = 'Fluid Content Elements (rcd_fsc_custom_css)';
+//         $itemArray[1] = 'EXT:rcd_fsc_custom_css/Configuration/TypoScript/';
+//         break;
+//     }
+// }
