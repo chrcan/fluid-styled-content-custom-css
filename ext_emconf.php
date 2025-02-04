9d5ede24af1ade3e7a3c06ae742fed005ba3da05
Christian <werbegrafik@rc-design.at>
2025-02-04 17:00:21 +0100

    include TYPO3 v13.4 php 8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,8 +31,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'fluid_styled_content' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
+            'fluid_styled_content' => '11.5.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
