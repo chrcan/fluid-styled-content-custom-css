8d7b553df79f6d32623959895cbc92d93325c08f
Christian <werbegrafik@rc-design.at>
2025-02-06 17:40:15 +0100

    composer fix

--- Classes/Form/Element/CssFieldElement.php
+++ Classes/Form/Element/CssFieldElement.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * This file is part of the rcdesign/fluid-styled-content-custom-css package.
+ *
+ * Copyright (C) 2025  rc design visual concepts <https://rc-design.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 namespace Rcdesign\RcdFscCustomCss\Form\Element;
 
 use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
